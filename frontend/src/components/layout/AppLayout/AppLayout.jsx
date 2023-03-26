bd25b4427195679dab59409e1c7777a5b850cb27
Alex Smirnov <ggrelaxi@gmail.com>
2023-03-27 02:07:50 +0300

    delete index.js imports

--- frontend/src/components/layout/AppLayout/AppLayout.jsx
+++ frontend/src/components/layout/AppLayout/AppLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Main } from '../Main';
-import { Header } from '../Header';
-import { Footer } from '../Footer';
+import { Main } from '../Main/Main';
+import { Header } from '../Header/Header';
+import { Footer } from '../Footer/Footer';
 import { LayoutContainer } from './layout.styled';
 
 export const AppLayout = () => {
